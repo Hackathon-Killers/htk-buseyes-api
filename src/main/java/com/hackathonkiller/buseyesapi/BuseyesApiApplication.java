1e5defbee25e19955e18e12e378f6408228447b9
권대환 <bigring@wemakeprice.com>
2020-12-06 23:31:41 +0900

    dh-dev: 프로젝트 생성

---
+++ src/main/java/com/hackathonkiller/buseyesapi/BuseyesApiApplication.java
@@ -0,0 +1,13 @@
+package com.hackathonkiller.buseyesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuseyesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuseyesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackathonkiller/buseyesapi/BuseyesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathonkiller.buseyesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuseyesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
